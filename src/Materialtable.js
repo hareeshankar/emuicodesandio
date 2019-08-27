5cf8638da02cf3ff7d91b183771cb8dfe8064a9d
Harish <hareeshankar@gmail.com>
2019-08-27 18:41:03 +0530

    added material table
    added material table

---
+++ src/Materialtable.js
@@ -0,0 +1,59 @@
+import React from "react";
+import MaterialTable from "material-table";
+
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: "Event Name", field: "eventname" },
+      { title: "Date", field: "eventdate" },
+      { title: "Location", field: "eventloc" },
+      { title: "Description", field: "eventdes" }
+    ],
+    data: [
+      { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
+      {
+        eventname: "Hospitality",
+        eventdate: "2019-08-27",
+        eventloc: "Chennai",
+        eventdes: "Manage hospitality for Shravans Wedding"
+      }
+    ]
+  });
+
+  return (
+    <MaterialTable
+      title="Events"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.push(newData);
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data[data.indexOf(oldData)] = newData;
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.splice(data.indexOf(oldData), 1);
+              setState({ ...state, data });
+            }, 600);
+          })
+      }}
+    />
+  );
+}
